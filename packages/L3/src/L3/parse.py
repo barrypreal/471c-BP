7b293b241ecb29194999b70eb37daf8eb8451fe7
Barry Peirson <barryp@udel.edu>
2026-03-04 13:52:00 -0500

    first pass of parse stuff

--- packages/L3/src/L3/parse.py
+++ packages/L3/src/L3/parse.py
@@ -1,6 +1,12 @@
+from collections.abc import Sequence
+from pathlib import Path
+
 from lark import Lark, Token, Transformer
+from lark.visitors import v_args  # pyright: ignore[reportUnknownVariableType]
 
 from .syntax import (
+    Identifier,
+    Let,
     Program,
     Term,
 )
@@ -13,20 +19,73 @@ class AstTransformer(Transformer[Token, Program | Term]):
         _program: Token,
         parameters: Sequence[Identifier],
         body: Term,
-    ) -> Program(
-        parameters=parameters,
-        body=body,
-    )
+    ) -> Program:
+        return Program(
+            parameters=parameters,
+            body=body,
+        )
 
-    @v_args(inLine=True)
-    def parameters (
-            self,
-            parameters: Sequence[Identifier]
-    )
+    def parameters(
+        self,
+        parameters: Sequence[Identifier],
+    ) -> Sequence[Identifier]:
+        return parameters
+
+    @v_args(inline=True)
+    def term(
+        self,
+        term: Term,
+    ) -> Term:
+        return term
+
+    @v_args(inline=True)
+    def let(
+        self,
+        _let: Token,
+        bindings: Sequence[tuple[Identifier, Term]],
+        body: Term,
+    ) -> Term:
+        return Let(
+            bindings=bindings,
+            body=body,
+        )
+
+    @v_args(inline=True)
+    def letrec(
+        self,
+        _letrec: Token,
+        bindings: Sequence[tuple[Identifier, Term]],
+        body: Term,
+    ) -> Term:
+        return Let(
+            bindings=bindings,
+            body=body,
+        )
+
+    def bindings(
+        self,
+        bindings: Sequence[tuple[Identifier, Term]],
+    ) -> Sequence[tuple[Identifier, Term]]:
+        return bindings
+
+    @v_args(inline=True)
+    def binding(
+        self,
+        name: Identifier,
+        value: Term,
+    ) -> tuple[Identifier, Term]:
+        return name, value
 
 
 def parse_term(source: str) -> Term:
-    grammer = Path(__file__).with_name("L3.lark").read_text()
+    grammar = Path(__file__).with_name("L3.lark").read_text()
     parser = Lark(grammar, start="term")
-    tree = parser.parse(source)
-    return AstTransformer()
+    tree = parser.parse(source)  # pyright: ignore[reportUnknownMemberType]
+    return AstTransformer().transform(tree)  # pyright: ignore[reportReturnType]
+
+
+def parse_program(source: str) -> Program:
+    grammar = Path(__file__).with_name("L3.lark").read_text()
+    parser = Lark(grammar, start="program")
+    tree = parser.parse(source)  # pyright: ignore[reportUnknownMemberType]
+    return AstTransformer().transform(tree)  # pyright: ignore[reportReturnType]
